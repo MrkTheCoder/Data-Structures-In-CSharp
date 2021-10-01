2105f87498a62d9bd8970ce86ddbecc3a24edb7c
MRK <mrezak@live.com>
2021-10-01 19:15:28 +0300

    Array - Step 3 - RemoveAt + UnitTests

--- 001Array/ArrayUnitTests/ArrayTests.cs
+++ 001Array/ArrayUnitTests/ArrayTests.cs
@@ -7,17 +7,18 @@
  *
  */
 
-using DataStructureArray;
+using System;
 using Xunit;
+using Array = DataStructureArray.Array;
 
 namespace ArrayUnitTests
 {
     public class ArrayTests
     {
-        #region [STEP 1 of Array class]
+        #region [Tests for STEP 1 of Array class]
         [Fact]
         // For "Method Naming Conventions", I used this format: "ClassMethodName_StateUnderTest_ExpectedBehavior".
-        public void Constructor_SetWithValidLength_ArrayCreatedWithProperLengthAndCount()
+        public void Constructor_SetValidLength_ArrayCreatedWithProperLengthAndCount()
         {
             // Arrange
             var arrayLength = 10;
@@ -30,9 +31,202 @@ namespace ArrayUnitTests
             // Assert
             Assert.Equal(arrayLength, array.Items.Length);
             Assert.Equal(noItem, array.Count);
-        } 
+        }
+
+        [Theory()]
+        [InlineData(0)]
+        [InlineData(-1)]
+        // For "Method Naming Conventions", I used this format: "Should_ExpectedBehavior_When_StateUnderTest".
+        public void ShouldConstructor_ThrowArgumentException_WhenLengthIsInvalid(int invalidLength)
+        {
+            // Arrange
+            Array array;
+            var exceptionMessage = "length cannot be less than 1";
+
+            // Act
+            // Assert
+            var ex = Assert.Throws<ArgumentException>(() => array = new Array(invalidLength));
+            Assert.Equal(exceptionMessage, ex.Message);
+        }
         #endregion
 
+
+        #region [Tests for STEP 2 of Array class]
+        [Fact]
+        public void Insert_InsertOneItem_AddedToArrayAndCountShowIt()
+        {
+            // Arrange
+            var arrayLength = 3;
+            var item = 100;
+            var expectedCount = 1;
+            Array array = new Array(arrayLength);
+
+            // Act
+            array.Insert(item);
+
+            //Assert
+            Assert.Equal(item, array.Items[0]);
+            Assert.Null(array.Items[1]);
+            Assert.True(array.Count == expectedCount);
+            Assert.True(array.Items.Length == arrayLength);
+        }
+
+        [Fact]
+        public void Insert_InsertTwoItems_AddedToArrayAndCountShowIt()
+        {
+            // Arrange
+            var arrayLength = 3;
+            var items = new int[] { 100, 102};
+            Array array = new Array(arrayLength);
+
+            // Act
+            array.Insert(items[0]);
+            array.Insert(items[1]);
+
+            //Assert
+            Assert.Equal(items[0], array.Items[0]);
+            Assert.Equal(items[1], array.Items[1]);
+            Assert.Null(array.Items[2]);
+            Assert.True(array.Count == items.Length);
+            Assert.True(array.Items.Length == arrayLength);
+        }
+
+        [Fact]
+        public void Insert_MakeArrayFull_NoArrayLengthChanges()
+        {
+            // Arrange
+            var arrayLength = 2;
+            var items = new int[] { 100, 102};
+            Array array = new Array(arrayLength);
+
+            // Act
+            // Using Fluent Interface
+            array.Insert(100).Insert(102);
+
+            //Assert
+            Assert.Equal(items[0], array.Items[0]);
+            Assert.Equal(items[1], array.Items[1]);
+            Assert.True(array.Count == items.Length);
+            Assert.True(array.Items.Length == arrayLength);
+        }
+
+        [Theory]
+        [InlineData(1, new []{100, 102})]
+        [InlineData(2, new []{100, 102, 103})]
+        [InlineData(3, new []{100, 102, 103, 104})]
+        [InlineData(4, new []{100, 102, 103, 104, 105})]
+        public void Insert_ByPassArrayLength_ExpandArray(
+            int arrayLength, 
+            int[] items)
+        {
+            // Arrange
+            var newLength = arrayLength + (arrayLength == 1 ? 1 : arrayLength / 2);
+            Array array = new Array(arrayLength);
+
+            // Act
+            foreach (var item in items)
+                array.Insert(item);    
+            
+            //Assert
+            for (int i = 0; i < items.Length; i++)
+                Assert.Equal(items[i], array.Items[i]);
+            Assert.True(array.Count == items.Length);
+            Assert.True(array.Items.Length == newLength);
+            // If there are extra free rooms they must be zero
+            for (int i = items.Length; i < newLength; i++) 
+                Assert.Null(array.Items[i]);
+        }
+        #endregion
+
+        #region [Tests for STEP 3 of Array class]
+        [Fact]
+        public void RemoveAt_IndexOfFirstItem_RemoveItemAndShiftOthersLeft()
+        {
+            // Arrange
+            var arrayLength = 2;
+            Array array = new Array(arrayLength);
+
+            // Act
+            array.Insert(100).Insert(102);
+            array.RemoveAt(0);
+
+            //Assert
+            Assert.Equal(102, array.Items[0]);
+            Assert.Null(array.Items[1]);
+            Assert.True(array.Count == 1);
+            Assert.True(array.Items.Length == arrayLength);
+        }
+
+        [Fact]
+        public void RemoveAt_IndexOfLastItem_RemoveItemAndMakeItNull()
+        {
+            // Arrange
+            var arrayLength = 2;
+            Array array = new Array(arrayLength);
+
+            // Act
+            array.Insert(100).Insert(102);
+            array.RemoveAt(1);
+
+            //Assert
+            Assert.Equal(100, array.Items[0]);
+            Assert.Null(array.Items[1]);
+            Assert.True(array.Count == 1);
+            Assert.True(array.Items.Length == arrayLength);
+        }
+
+        [Fact]
+        public void RemoveAt_RemoveAll_AllItemsShouldBeNull()
+        {
+            // Arrange
+            var arrayLength = 2;
+            Array array = new Array(arrayLength);
+
+            // Act
+            array.Insert(100).Insert(102);
+            array.RemoveAt(0).RemoveAt(0);
+
+            //Assert
+            Assert.Null(array.Items[0]);
+            Assert.Null(array.Items[1]);
+            Assert.True(array.Count == 0);
+            Assert.True(array.Items.Length == arrayLength);
+        }
+
+        [Fact]
+        public void RemoveAt_InvalidIndexPointToEmptyRoom_ThrowArgumentException()
+        {
+            // Arrange
+            var arrayLength = 2;
+            Array array = new Array(arrayLength);
+
+            // Act
+            array.Insert(100);
+
+            //Assert
+            var ex = Assert.Throws<ArgumentException>(() => array.RemoveAt(1));
+            Assert.Equal("Index is out of range!", ex.Message);
+            Assert.True(array.Count == 1);
+            Assert.True(array.Items.Length == arrayLength);
+        }
+
+        [Fact]
+        public void RemoveAt_NegativeIndex_ThrowArgumentException()
+        {
+            // Arrange
+            var arrayLength = 2;
+            Array array = new Array(arrayLength);
+
+            // Act
+            array.Insert(100).Insert(102);
+
+            //Assert
+            var ex = Assert.Throws<ArgumentException>(() => array.RemoveAt(-1));
+            Assert.Equal("Index is out of range!", ex.Message);
+            Assert.True(array.Count == 2);
+            Assert.True(array.Items.Length == arrayLength);
+        }
+        #endregion
     }
 
     
--- 001Array/DataStructureArray/Array.cs
+++ 001Array/DataStructureArray/Array.cs
@@ -12,29 +12,37 @@
  *      Delete	            O(n)
  *
  * Practice One:
- *      We will write an Array class that can hold integer numbers in an array and it will expand automatically if we insert more than its
- *      first length that we set at class instantiation. (then we will change this class to support generic array).
+ *      We will write an Array class that can hold integer numbers in a nullable array. It will expand automatically, if we insert more than its
+ *      length. For this exercise, please follow my below steps. After each step, check your code with mine. 
  *
  *      Please follow each step sequentially:
  *          1) Build Array class with these conditions:
- *              *. Members (You will add more fields and properties based on new requirements, when you go to next steps):
- *                  A public property of type int[] with private setter. (We will store our numbers in this array)
- *                  A public property of type int with private setter. (keep track of total inserted numbers and latest free array index)
+ *              *. Members:
+ *                  public property "Items" of type int?[] with private setter. (We will store our numbers in this array)
+ *                  public property "Count" of type int with private setter. (keep track of total inserted numbers and latest free array index)
  *              *. At instantiation, it should ask for array length.
- *              *. Verify that length is valid.
+ *              *. Verify that length is valid. throw an exception if it is not.
+ *                      [T]. Write Unit Test.
  *
  *          2) Add public Insert method in these steps:
- *              *.  1st) It accepts an Item of type int and add it to the array.
- *                      *. Write the first Unit Test to make sure it work properly.
- *              *.  2nd) Expand array 50% of its initialed length, if it becomes full. We need to store value of first instantiated length.
- *                      (You can change this rule based on your desire.)
- *                      *. Write Unit Test.
- *              *.  3) Refactor your code to support Single Responsibility Principle (SRP) on this method. (Also it will cover Don't Repeat
- *                  Yourself [DRY] principle too)
- *                      *. Run all previous Unit Test to make sure everything go smoothly.
- *              *.  4) Support Fluent Interface design pattern. (What is Fluent Interface design patter is:
- *                  https://dotnettutorials.net/lesson/fluent-interface-design-pattern/)
- *                      *. Run all previous Unit Test to make sure everything go smoothly.
+ *              1st) It has a parameter: Item of type int. It will add it to the array.
+ *                      [T]. Write the first Unit Test to make sure it work properly.
+ *              2nd) Expand array 50% of its initialed length, if it becomes full. We need to store value of first instantiated length.
+ *                   (You can change this rule based on your desire.)
+ *                      [T]. Write Unit Tests.
+ *              3ed) Refactor your code to support Single Responsibility Principle (SRP) on this method. (Also it will cover Don't Repeat
+ *                   Yourself [DRY] principle too)
+ *                      [T]. Run all previous Unit Test to make sure everything go smoothly.
+ *              4th) Support Fluent Interface design pattern. (What is Fluent Interface design patter is:
+ *                   https://dotnettutorials.net/lesson/fluent-interface-design-pattern/)
+ *                      [T]. Run all previous Unit Test to make sure everything go smoothly.
+ *              5th) What is each method Time Complexity?
+ *
+ *          3) Add public RemoveAt method:
+ *              *. It has a parameter: Index of type int. This is the index of one of the inserted items.
+ *              *. HINT: check for index to be valid based on total inserted items. throw an exception if it is not.
+ *                  [T]. Write Unit Test.
+ *              *. What is method Time Complexity?
  */
 
 using System;
@@ -43,10 +51,10 @@ namespace DataStructureArray
 {
     public class Array
     {
-        #region [Step 1]
+        #region [STEP 1]
         private readonly int _initialLength; // [Step 2]
-        
-        public int[] Items { get; private set; }
+
+        public int?[] Items { get; private set; }
         public int Count { get; private set; }
 
         public Array(int length)
@@ -54,17 +62,16 @@ namespace DataStructureArray
             // length should not be less than +1.
             if (length < 1)
                 throw new ArgumentException("length cannot be less than 1");
-            
-            _initialLength = length; // [Step 2]
-            Items = new int[length];
+
+            _initialLength = length ; // [Step 2]
+            Items = new int?[length];
             Count = 0;
         }
         #endregion
 
-
-        #region [Step 2]
+        #region [STEP 2]
         // We are returning the Array class to support "Fluent Interface" design pattern.
-        public Array Insert(int number)
+        public Array Insert(int number) // O(1) no expanding --- O(n) Expanding
         {
             // Extract logic of expanding array out of this to support DRY and SRP principles.
             ExpandArray();
@@ -75,25 +82,38 @@ namespace DataStructureArray
             return this;
         }
 
-
         // Extracted logic of expanding array.
-        private void ExpandArray()
+        private void ExpandArray() // O(n)
         {
             if (Count == Items.Length)
             {
                 // Create new array with current length plus half of first length at instantiation of this class.
-                var expandedArray = new int[Count+(_initialLength/2)];
+                // If _initialLength = 1 can make trouble! ;) Why!?
+                var newLength = Count + (_initialLength == 1 ? 1 : _initialLength / 2);
+                var expandedArray = new int?[newLength];
 
                 // Cloning current array to new expanded array.
                 for (int i = 0; i < Count; i++)
                     expandedArray[i] = Items[i];
 
-                // set current array to new expanded array with its extra rooms.
                 Items = expandedArray;
             }
         }
         #endregion
 
+        #region [STEP 3]
+        public Array RemoveAt(int index) // O(n)
+        {
+            if (index < 0 || index >= Count)
+                throw new ArgumentException("Index is out of range!");
+
+            for (int i = index; i < Count; i++)
+                Items[i] = i + 1 < Count ? Items[i + 1] : null;
+            
+            Count--;
+            return this;
+        }
+        #endregion
     }
 
 }
