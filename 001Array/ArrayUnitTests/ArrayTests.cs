f04b0ca25f7c8e4873284fea9d158add24371f16
MRK <mrezak@live.com>
2021-10-01 14:00:53 +0300

    UnitTest - Constructor Testing

---
+++ 001Array/ArrayUnitTests/ArrayTests.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * MAKE SURE TO ADD xUnit FROM Nuget TO THIS PROJECT.
+ *
+ * As you know there are many "Method Naming Conventions" out there, I will using two "Method Naming Conventions" that I like and use more at
+ * below tests. I will do that JUST for learning purpose, But in real world, we should we should only stick to one naming convention in our
+ * project based on our desire or team.
+ *
+ */
+
+using DataStructureArray;
+using Xunit;
+
+namespace ArrayUnitTests
+{
+    public class ArrayTests
+    {
+        #region [STEP 1 of Array class]
+        [Fact]
+        // For "Method Naming Conventions", I used this format: "ClassMethodName_StateUnderTest_ExpectedBehavior".
+        public void Constructor_SetWithValidLength_ArrayCreatedWithProperLengthAndCount()
+        {
+            // Arrange
+            var arrayLength = 10;
+            var noItem = 0;
+            Array array;
+
+            // Act
+            array = new Array(arrayLength);
+
+            // Assert
+            Assert.Equal(arrayLength, array.Items.Length);
+            Assert.Equal(noItem, array.Count);
+        } 
+        #endregion
+
+    }
+
+    
+}
--- 001Array/DataStructureArray/Array.cs
+++ 001Array/DataStructureArray/Array.cs
@@ -12,18 +12,29 @@
  *      Delete	            O(n)
  *
  * Practice One:
- *      Try to write Array class that can hold integer numbers in an array. Follow each step sequentially:
+ *      We will write an Array class that can hold integer numbers in an array and it will expand automatically if we insert more than its
+ *      first length that we set at class instantiation. (then we will change this class to support generic array).
+ *
+ *      Please follow each step sequentially:
  *          1) Build Array class with these conditions:
- *              *. Two members:
- *                  A private field of type int[]. (We will store our numbers in this array)
+ *              *. Members (You will add more fields and properties based on new requirements, when you go to next steps):
+ *                  A public property of type int[] with private setter. (We will store our numbers in this array)
  *                  A public property of type int with private setter. (keep track of total inserted numbers and latest free array index)
  *              *. At instantiation, it should ask for array length.
  *              *. Verify that length is valid.
- *          2) Add public Insert method in these 3 steps:
- *              *. 1) It accepts an Item of type int and add it to the array.
- *              *. 2) Expand array 50% of its initialed length, if it becomes full. (You can change this rule based on your desire.)
- *              *. 3) Apply Single Responsibility Principle (SRP) on this method. (Also it will cover Don't Repeat Yourself [DRY] principle too)
- *              *. 4) Support Fluent Interface design pattern. (What is Fluent Interface design patter is: https://dotnettutorials.net/lesson/fluent-interface-design-pattern/)
+ *
+ *          2) Add public Insert method in these steps:
+ *              *.  1st) It accepts an Item of type int and add it to the array.
+ *                      *. Write the first Unit Test to make sure it work properly.
+ *              *.  2nd) Expand array 50% of its initialed length, if it becomes full. We need to store value of first instantiated length.
+ *                      (You can change this rule based on your desire.)
+ *                      *. Write Unit Test.
+ *              *.  3) Refactor your code to support Single Responsibility Principle (SRP) on this method. (Also it will cover Don't Repeat
+ *                  Yourself [DRY] principle too)
+ *                      *. Run all previous Unit Test to make sure everything go smoothly.
+ *              *.  4) Support Fluent Interface design pattern. (What is Fluent Interface design patter is:
+ *                  https://dotnettutorials.net/lesson/fluent-interface-design-pattern/)
+ *                      *. Run all previous Unit Test to make sure everything go smoothly.
  */
 
 using System;
@@ -32,10 +43,10 @@ namespace DataStructureArray
 {
     public class Array
     {
-        private int[] _array;
-        private readonly int _initialLength;
-
-        public int Length => _array.Length;
+        #region [Step 1]
+        private readonly int _initialLength; // [Step 2]
+        
+        public int[] Items { get; private set; }
         public int Count { get; private set; }
 
         public Array(int length)
@@ -44,18 +55,21 @@ namespace DataStructureArray
             if (length < 1)
                 throw new ArgumentException("length cannot be less than 1");
             
-            _initialLength = length;
-            _array = new int[length];
+            _initialLength = length; // [Step 2]
+            Items = new int[length];
             Count = 0;
         }
-        
+        #endregion
+
+
+        #region [Step 2]
         // We are returning the Array class to support "Fluent Interface" design pattern.
         public Array Insert(int number)
         {
             // Extract logic of expanding array out of this to support DRY and SRP principles.
             ExpandArray();
 
-            _array[Count++] = number;
+            Items[Count++] = number;
 
             // this line will support "Fluent Interface" design pattern. (chain of Methods)
             return this;
@@ -65,19 +79,20 @@ namespace DataStructureArray
         // Extracted logic of expanding array.
         private void ExpandArray()
         {
-            if (Count == Length)
+            if (Count == Items.Length)
             {
                 // Create new array with current length plus half of first length at instantiation of this class.
                 var expandedArray = new int[Count+(_initialLength/2)];
 
                 // Cloning current array to new expanded array.
-                for (int i = 0; i < Length; i++)
-                    expandedArray[i] = _array[i];
+                for (int i = 0; i < Count; i++)
+                    expandedArray[i] = Items[i];
 
                 // set current array to new expanded array with its extra rooms.
-                _array = expandedArray;
+                Items = expandedArray;
             }
         }
+        #endregion
 
     }
 
