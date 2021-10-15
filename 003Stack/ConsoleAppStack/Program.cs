fe16b5bf04e608c9d7f33059d26776d8beb2613d
MRK <mrezak@live.com>
2021-10-15 17:49:58 +0300

    Stack - Console App Done.

--- 003Stack/ConsoleAppStack/Program.cs
+++ 003Stack/ConsoleAppStack/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using StackDataStructure;
 
 namespace ConsoleAppStack
 {
@@ -6,7 +7,22 @@ namespace ConsoleAppStack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stack = new Stack<int>();
+            stack.Push(10).Push(20).Push(30);
+            Console.WriteLine("Stack items:");
+            Console.WriteLine(stack);
+            Console.WriteLine();
+            Console.WriteLine($"Pop last item in stack: {stack.Pop()}");
+            Console.WriteLine("Stack items:");
+            Console.WriteLine(stack);
+            Console.WriteLine();
+            Console.WriteLine($"Peek last item in stack: {stack.Peek()}");
+            Console.WriteLine($"Total Items in stack: {stack.Count}");
+            Console.WriteLine();
+            Console.WriteLine("Pop stack twice...");
+            stack.Pop();
+            stack.Pop();
+            Console.WriteLine($"Is stack empty: {stack.IsEmpty()}");
         }
     }
 }
