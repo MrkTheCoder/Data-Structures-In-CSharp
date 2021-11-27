8521453d55749b0974ef99234a71783a82660c1c
MRK <mrezak@live.com>
2021-11-27 16:41:35 +0300

    HashTable Project - Started Structure

---
+++ HashTableUnitTests/HastTableTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace HashTableUnitTests
+{
+    public class HastTableTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
